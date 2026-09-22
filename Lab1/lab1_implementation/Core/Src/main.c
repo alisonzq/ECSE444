d8d1b4c9aa0c736b04923f52f3c442ea17b4fac9
Alison Quach <alisonquato@gmail.com>
2026-09-22 18:31:22 -0400

    functioncalls

--- Lab1/lab1_implementation/Core/Src/main.c
+++ Lab1/lab1_implementation/Core/Src/main.c
@@ -25,6 +25,7 @@
 #include "arm_math.h"
 #include "kalman.h"
 #include "math.h"
+#define LENGTH 100
 
 /* USER CODE END Includes */
 
@@ -146,72 +147,72 @@ int main(void)
     }
 
     return 0;
-}
+  }
 
-int kalman(kalman_state *kstate, float measurement) {
-    kstate->p = kstate->p + kstate->q;
-    kstate->k = kstate->p / (kstate->p + kstate->r);
-    kstate->x = kstate->x + kstate->k * (measurement - kstate->x);
-    kstate->p = (1 - kstate->k) * kstate->p;
-    return 0;
-}
+  int kalman(kalman_state *kstate, float measurement) {
+      kstate->p = kstate->p + kstate->q;
+      kstate->k = kstate->p / (kstate->p + kstate->r);
+      kstate->x = kstate->x + kstate->k * (measurement - kstate->x);
+      kstate->p = (1 - kstate->k) * kstate->p;
+      return 0;
+  }
 
-//Subtraction of original and data obtained by Kalman filter tracking.
-int substract(float* InputArray, float* OutputArray, float* DifferenceArray, int Length) {
+  //Subtraction of original and data obtained by Kalman filter tracking.
+  int substract(float* InputArray, float* OutputArray, float* DifferenceArray, int Length) {
     for (int i = 0; i < Length; i++) {
         DifferenceArray[i] = InputArray[i] - OutputArray[i];
     }
     return 0;
-}
+  }
 
-//Calculation of the standard deviation and the average of the difference obtained in a).
-int compute_stddev_and_avg(float* DifferenceArray, float* average, float* standard_deviation, int Length) {
-    float sum = 0;
-    for (int i = 0; i < Length; i++) {
-        sum += DifferenceArray[i];
-    }
-    *average = sum / Length;
+  //Calculation of the standard deviation and the average of the difference obtained in a).
+  int compute_stddev_and_avg(float* DifferenceArray, float* average, float* standard_deviation, int Length) {
+      float sum = 0;
+      for (int i = 0; i < Length; i++) {
+          sum += DifferenceArray[i];
+      }
+      *average = sum / Length;
 
-    float variance_sum = 0;
-    for (int i = 0; i < Length; i++) {
-        variance_sum += (DifferenceArray[i] - *average) * (DifferenceArray[i] - *average);
-    }
-    *standard_deviation = sqrt(variance_sum / Length);
-    return 0;
-}
+      float variance_sum = 0;
+      for (int i = 0; i < Length; i++) {
+          variance_sum += (DifferenceArray[i] - *average) * (DifferenceArray[i] - *average);
+      }
+      *standard_deviation = sqrt(variance_sum / Length);
+      return 0;
+  }
 
-//Calculation of the correlation between the original and tracked vectors. 
-int correlate(float* InputArray, float* OutputArray, float* CorrResult, int Length) {
-    int result_length = 2 * Length - 1;
-    for (int n = 0; n < result_length; n++) {
-        float sum = 0;
-        int shift = n - (Length - 1);
-        for (int i = 0; i < Length; i++) {
-            int j = i + shift;
-            if (j >= 0 && j < Length) {
-                sum += InputArray[i] * OutputArray[j];
-            }
-        }
-        CorrResult[n] = sum;
-    }
-    return 0;
-}
+  //Calculation of the correlation between the original and tracked vectors. 
+  int correlate(float* InputArray, float* OutputArray, float* CorrResult, int Length) {
+      int result_length = 2 * Length - 1;
+      for (int n = 0; n < result_length; n++) {
+          float sum = 0;
+          int shift = n - (Length - 1);
+          for (int i = 0; i < Length; i++) {
+              int j = i + shift;
+              if (j >= 0 && j < Length) {
+                  sum += InputArray[i] * OutputArray[j];
+              }
+          }
+          CorrResult[n] = sum;
+      }
+      return 0;
+  }
 
-//Calculation of the convolution between the two vectors.
-int convolve(float* InputArray, float* OutputArray, float* ConvResult, int Length) {
-    int result_length = 2 * Length - 1;
-    for (int n = 0; n < result_length; n++) {
-        float sum = 0;
-        for (int i = 0; i < Length; i++) {
-            int j = n - i;
-            if (j >= 0 && j < Length) {
-                sum += InputArray[i] * OutputArray[j];
-            }
-        }
-        ConvResult[n] = sum;
-    }
-    return 0;
-}
+  //Calculation of the convolution between the two vectors.
+  int convolve(float* InputArray, float* OutputArray, float* ConvResult, int Length) {
+      int result_length = 2 * Length - 1;
+      for (int n = 0; n < result_length; n++) {
+          float sum = 0;
+          for (int i = 0; i < Length; i++) {
+              int j = n - i;
+              if (j >= 0 && j < Length) {
+                  sum += InputArray[i] * OutputArray[j];
+              }
+          }
+          ConvResult[n] = sum;
+      }
+      return 0;
+  }
 
   /* USER CODE END 2 */
 
@@ -229,9 +230,19 @@ int convolve(float* InputArray, float* OutputArray, float* ConvResult, int Lengt
     };
 
     float InputArray[] = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0};
-    float OutputArray[10];
+    float OutputArray[LENGTH];
 
-    Kalmanfilter(InputArray, OutputArray, &ks, 10);
+    Kalmanfilter(InputArray, OutputArray, &ks, LENGTH);
+
+    float DifferenceArray[LENGTH];
+    substract(InputArray, OutputArray, DifferenceArray, LENGTH);
+    compute_stddev_and_avg(DifferenceArray, &average, &standard_deviation, LENGTH);
+
+    //correlation and convolution slide one array across the other across every possible overlap position
+    float CorrResult[2*LENGTH-1];
+    float ConvResult[2*LENGTH-1];
+    correlate(InputArray, OutputArray, CorrResult, 2*LENGTH-1);
+    convolve(InputArray, OutputArray, ConvResult, 2*LENGTH-1);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
